dc769b00cad754a2e243f27f5a7548a58667ef8b
MD2990 <maz2826maz@gmail.com>
2021-01-22 14:05:19 +0400

    Added bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Col, Container, ProgressBar } from 'react-bootstrap';
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
+		<>
 			<Head>
 				<title>Create Next App</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<h1>Hi to all</h1>
-		</div>
+
+			<Container>
+				<Row className='justify-content-md-center border border-black '>
+					<Col xs>1 of 3</Col>
+					<Col>2 of 3</Col>
+					<Col>3 of 3</Col>
+				</Row>
+				<Row>
+					<Col>1 of 3</Col>
+					<Col>2 of 3</Col>
+					<Col>3 of 3</Col>
+				</Row>
+			</Container>
+			{/* 	{ <div className={styles.container}>
+				<Button variant='primary' onClick={() => alert('Hi')}>
+					{' '}
+					Click Me
+				</Button>
+			</div> } */}
+		</>
 	);
 }
