75dd6e4e2c4e5f9b54446cd03ad9d5d89641c0de
MD2990 <oman2990@gmail.com>
2021-02-10 00:27:07 +0400

    table footer total bug

--- components/ReactTabulator.js
+++ components/ReactTabulator.js
@@ -1,33 +1,31 @@
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
+import Chance from 'chance';
+import cellEditFactory from 'react-bootstrap-table2-editor';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
 const { SearchBar, ClearSearchButton } = Search;
 
-var Chance = require('chance');
-var chance = new Chance();
-
-// Instantiate Chance so it can be used
-
-// Use Chance here.
-
-//console.log(my_random_string);
-
 const products = [];
 
-for (let index = 0; index < 999999; index++) {
-	let my_random_string = chance.name();
-	let my_random_price = chance.floating({ min: 0, max: 100, fixed: 4 });
-	let my_random_number = chance.integer({ min: 10, max: 999999 });
+const chance = new Chance();
+
+for (let index = 0; index < 15; index++) {
+	const my_random_string = chance.name();
+	const my_random_price = chance.integer({ min: 0, max: 50, fixed: 0 });
+	//const my_random_number = chance.integer({ min: 10, max: 999999 });
 
 	products.push({
-		id: index,
+		id: index + 1,
 		name: my_random_string,
-		price: my_random_price,
+		price: index + 15,
 	});
 }
+
+let total = products.reduce((acc, pilot) => acc + pilot.price, 0);
 
 const columns = [
 	{
@@ -46,14 +44,30 @@ const columns = [
 		dataField: 'price',
 		sort: true,
 		text: 'Product Price',
-		footerTitle: (column, colIndex) =>
-			`this is custom title for ${column.text}`,
+		/* 	footerTitle: (column, colIndex) =>
+			`this is custom title for ${column.text}`, */
 
-		footer: (columnData) =>
-			'$$ ' + columnData.reduce((acc, item) => acc + item, 0),
+		footer: (columnData) => columnData.reduce((acc, item) => acc + item, 0),
 	},
 ];
 
+function priceFormatter(column, colIndex, { text }) {
+	return (
+		<h5>
+			<strong>$$ {(column.text = total)} $$</strong>
+		</h5>
+	);
+}
+function beforeSaveCell(oldValue, newValue, row, column, done) {
+	setTimeout(() => {
+		if (confirm('Do you want to accep this change?')) {
+			done(true);
+		} else {
+			done(false);
+		}
+	}, 0);
+	return { async: true };
+}
 const defaultSorted = [
 	{
 		dataField: 'id',
@@ -80,6 +94,16 @@ const MyTable = () => {
 							columns={columns}
 							defaultSorted={defaultSorted}
 							pagination={paginationFactory()}
+							cellEdit={cellEditFactory({
+								mode: 'click',
+								onStartEdit: (row, column, rowIndex, columnIndex) => {
+									console.log('start to edit!!!');
+								},
+								beforeSaveCell: (oldValue, newValue, row, column) => {
+									console.log('Before Saving Cell!!');
+								},
+								afterSaveCell: (oldValue, newValue, row, columnm) => {},
+							})}
 						/>
 					</div>
 				)}
@@ -99,22 +123,22 @@ const MyTable = () => {
 };
 
 function print() {
-	/* 	doc.autoTable({
-		head: [['Name', 'Email', 'Country']],
-		body: [
-			['David', 'david@example.com', 'Sweden'],
-			['Castille', 'castille@example.com', 'Spain'],
-			// ...
-		],
-	});
-	doc.save('table.pdf'); */
+	console.log(total);
 
+	const doc = new jsPDF();
+	const key = [['id', 'name', 'price']];
+	doc.autoTable({
+		head: key,
+		body: products.map((x) => Object.values(x)),
+	});
+	doc.save('table.pdf');
+	/* 
 	const doc = new jsPDF();
 	doc.autoTable({
 		styles: { fillColor: [255, 0, 0] },
 		html: '#my-table',
 	});
-	doc.save('table.pdf');
+	doc.save('table.pdf'); */
 }
 
 export default MyTable;
